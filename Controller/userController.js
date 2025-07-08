29e0d1f9c42b4f1ab0ae0cf4f55e400dae0e65e8
Willington Juma <willingtonejumamconyango@gmail.com>
2025-07-08 12:19:37 +0300

    the entire backend training code class

--- Controller/userController.js
+++ Controller/userController.js
@@ -1,5 +1,10 @@
 const User=require("../models/User")
 const bcrypt=require("bcryptjs")
+const jwt=require("jsonwebtoken")
+const generateToken =(userId)=>{
+    return jwt.sign({id:userId},process.env.JWT_SECRET,{ExpiresIn: "2d"})
+}
+
 const USerRegistration=async (req,res) => {
     try {
         const{username,email,password}=req.body
@@ -26,7 +31,18 @@ return res.status(201).json(newuser)
         console.log("registration failed",error)
     }
 }
+const GetAllUser=async (req,res) => {
+    try {
+        const user=await User.find()
+        if(!user){
+            return res.status(404).json({"message":"User not found"})
+        }
+        return res.status(201).json(user)
+    } catch (error) {
+       console.log  (error) 
+    }
+    
+}
 
 
-
-module.exports={USerRegistration}
+module.exports={USerRegistration,GetAllUser}
--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,7 +1,9 @@
 
 
 const { Router } = require("express")
-const {USerRegistration}=require("../Controller/userController")
+const {USerRegistration,GetAllUser}=require("../Controller/userController")
 const routes=Router()
 routes.post("/register",USerRegistration)
+routes.get("/get",GetAllUser)
+
 module.exports=routes
